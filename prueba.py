150f798cae771dc0dc5e4867b00dbfed9cec7e1f
BryantOrtega10 <bryant.ortega1010@gmail.com>
2022-11-15 23:20:34 -0500

    V 0.0.4 Logica Objetos de Sesion

--- prueba.py
+++ prueba.py
@@ -3,14 +3,12 @@ from app.objetos_sesion.PlatoFuerte import PlatoFuerte
 from app.objetos_sesion.Bebida import Bebida
 from app.objetos_sesion.Menu import Menu
 from app.objetos_sesion.ItemCarrito import ItemCarrito
-
-
 from app.objetos_sesion.Carrito import Carrito
 
 
 adicion1 = Adicion(101, "ALBONDIGA", "albondiga.jpg", "unidad", 101, 1, 10, 5000)
 adicion2 = Adicion(13, "tocineta", "workinprogress", "gramos", 213, 1, 10, 9900)
-adicion3 = Adicion(13, "tocineta", "workinprogress", "gramos", 213, 1, 10, 9900)
+
 #producto_modificados_por_cliente
 platoFuerte1 = PlatoFuerte(101, "PASTA BOLOÑESA", 20000, "pastaBoloniesa.jpg", [], [], [adicion1, adicion2], [adicion1, adicion1], 0, False, 0, False)
 platoFuerte2 = PlatoFuerte(101, "PASTA BOLOÑESA", 20000, "pastaBoloniesa.jpg", [], [], [adicion1, adicion2], [adicion1, adicion2], 0, False, 0, False)
@@ -26,8 +24,8 @@ menu2 = Menu(1011, "COMBO MCCOMBO", "combomccombo.png", 25000, "En memoria", Non
 
 
 itemCarrito1 = ItemCarrito(1, menu1, "sede1", [bebida1, platoFuerte2])
-itemCarrito2 = ItemCarrito(2, menu2, "sede2", [platoFuerte4])
-itemCarrito3 = ItemCarrito(1, menu2, "sede2", [bebida1, platoFuerte3])
+itemCarrito2 = ItemCarrito(2, menu2, "sede2", [platoFuerte5])
+itemCarrito3 = ItemCarrito(3, menu2, "sede2", [bebida1, platoFuerte3])
 
 carrito = Carrito(1,0, [itemCarrito1, itemCarrito2, itemCarrito3], "cliente", None, "En memoria")
 print(carrito.calcularValor())
